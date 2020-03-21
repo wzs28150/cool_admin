4da0dff474e8652438dde4af542145ac499f809b
汪中帅 <wz666s@163.com>
2020-03-21 09:33:43 +0800

    Update Route.php

--- src/route/Route.php
+++ src/route/Route.php
@@ -5,7 +5,7 @@
  * @Author: wzs
  * @Date: 2020-03-20 14:58:40
  * @LastEditors: sueRimn
- * @LastEditTime: 2020-03-21 09:17:43
+ * @LastEditTime: 2020-03-21 09:28:00
  */
 
 use think\facade\Route;
@@ -13,7 +13,4 @@ use think\facade\Route;
 
 Route::group('admin', function () {
     Route::get('', 'wzs\cooladmin\controller\Index@index');
-    // Route::get('login', 'wzs\cooladmin\controller\Login@index');
-    // Route::get('captcha', 'wzs\cooladmin\controller\Login@captcha');
-    // Route::post('login', 'wzs\cooladmin\controller\Login@doLogin');
 });
